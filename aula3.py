08033babf223df1968990eaac275c421559d9bde
leticiagabrielli02@gmail.com <leticia>
2022-10-17 19:56:55 -0300

    terceira alteracao

--- aula3.py
+++ aula3.py
@@ -1,9 +1,50 @@
-print('hello world') 
-texto = ' informe o primeiro valor: ' 
+from curses import newwin
+from tkinter import *
 
-valor1 = int(input( texto) )
-valor2 = input ( 'informe o segundo valor: ' )
-print ( ' o primeiro valor informado foi:' , valor1, 
-       '\nO segundo valor informado foi: ',valor2)
+janela = Tk()
+janela["bg"] = "white"
+app = Frame(janela)
+app.grid()
+menuPrincipal = Menu(app)
+janela.config(menu=menuPrincipal)
 
-print ( int(valor1) + int(valor2))
+fileMenu = Menu(menuPrincipal)
+fileMenu.add_command(label="Cadastrar"
+            ,command=iniciarTelaCadastro)
+fileMenu.add_command(label="Cadastrar"
+            ,command=iniciarTelaCadastro)
+menuPrincipal.add_cascade(label="Funcao"
+                        ,menu=fileMenu)
+
+
+def escrever():
+    print("O meu nome é :",entryNome.get()
+        , "e minha idade é: ", entryIdade.get())
+    
+
+lblNome = Label(janela,text="Informe o seu nome: "
+            ,font="Times"
+            ,bg="white",foreground="black")
+lblNome.place(x=100,y=50)
+
+entryNome = Entry(janela)
+entryNome.place(x=230,y=55)
+
+lblIdade = Label(janela,text="Informe a sua Idade:"
+                ,font="Times"
+                ,bg="white", foreground="black")
+lblIdade.place(x=100, y=75)
+
+entryIdade = Entry(janela)
+entryIdade.place(x=230,y=80)
+
+btnFalarNome = Button(janela,width=40
+            ,text="Clicar", command=escrever)
+btnFalarNome.place(x=150,y=110)
+
+title = "Sistema Tarumã"
+janela.title(title)
+janela.geometry("800x600")
+janela.resizable(False,False)
+janela.mainloop()
+janela.destroy()
